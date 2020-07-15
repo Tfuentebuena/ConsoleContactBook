0e9c2305b88322450618e2570825fb476818b4b6
Tobias Fuentebuena Guardon <tfuentebuena@est.frba.utn.edu.ar>
2020-07-15 17:22:45 -0300

    custom file support added

--- App.py
+++ App.py
@@ -3,27 +3,35 @@ from sys import exit
 import time
 
 
-def operations(book):
+def is_csv(file_name):
+    if not file_name.endswith(".csv"):
+        file_name = file_name + ".csv"
+    return file_name
+
+
+def operations(book, file_name="contacts.csv"):
     dec = "y"
     while dec == "y" or dec == "Y":
-        print("What do you want to do?:\n(1) Add contact.\n(2) Update contact.\n"
-              "(3) Delete contact.\n(4) Search contact.\n(5) Exit.\n"
-              "Choose an answer: ")
-        x = int(input())
-        if type(x) != int or x > 5 or x < 1:
+        print(
+            "What do you want to do?:\n(1) Add contact.\n(2) Update contact.\n"
+            "(3) Delete contact.\n(4) Search contact.\n(5) Exit.\n"
+            "Choose an answer: ")
+        try:
+            x = int(input())
+        except ValueError:
             print("No operation found.")
             time.sleep(1)
             continue
         if x == 1:
-            data_csv = open("contacts.csv", "a")
+            data_csv = open(file_name, "a")
             book.add_contact(data_csv)
         elif x == 2:
-            book.update_contact()
+            book.update_contact(file_name)
         elif x == 3 or x == 4:
             if x == 3:
-                book.delete_contact()
+                book.delete_contact(file_name)
             else:
-                data_csv = open("contacts.csv", "r")
+                data_csv = open(file_name, "r")
                 book.search_contact(data_csv)
         elif x == 5:
             print("Closing...")
@@ -32,9 +40,19 @@ def operations(book):
 
 
 def main():
-    book = ContactBook()
-    operations(book)
+    print("The default file name is 'contacts.csv'.\nDo you want to use a "
+          "custom one? (Y/N): ")
+    custom_file = input()
+
+    if custom_file == "y" or custom_file == "Y":
+        customfile_name = input("Enter the name of the csv file: ")
+        file_name = is_csv(customfile_name)
+        book = ContactBook(file_name)
+        operations(book, file_name)
+    else:
+        book = ContactBook()
+        operations(book)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- Objects.py
+++ Objects.py
@@ -4,51 +4,56 @@ import os.path
 
 class ContactBook:
 
-    def __init__(self):
+    def __init__(self, filename="contacts.csv"):
         self.contacts = {"Name": None, "Address": None, "Phone": None,
                          "Email": None}
-        if not os.path.isfile("contacts.csv"):
-            with open("contacts.csv", "w") as data_csv:
+        if not os.path.isfile(filename):
+            with open(filename, "w") as data_csv:
                 writer = csv.DictWriter(data_csv, delimiter=",",
                                         fieldnames=self.contacts)
                 writer.writeheader()
 
-    def add_contact(self, data_csv):
+    def add_contact(self, filename):
         data = ["Name", "Address", "Phone", "Email"]
         data_list = [None, None, None, None]
         for i in range(4):
             data_list[i] = input("Enter the {}: ".format(data[i]))
-        data_writer = csv.writer(data_csv, delimiter=",")
+        data_writer = csv.writer(filename, delimiter=",")
         data_writer.writerow(data_list)
         return None
 
-    def delete_contact(self):
+    def delete_contact(self, filename):
         contact = input("Which contact do you want to delete?: ")
-        with open("contacts.csv", "r") as data_csv:
+        with open(filename, "r") as data_csv:
             data_reader = csv.reader(data_csv, delimiter=",")
             data_aux = []
             for row in data_reader:
                 data_aux.append(row)
-        with open("contacts.csv", "w") as data_csv:
+        with open(filename, "w") as data_csv:
             data_writer = csv.writer(data_csv, delimiter=",")
             for row in data_aux:
                 if row[0].lower() != contact.lower():
                     data_writer.writerow(row)
         return None
 
-    def search_contact(self, data_csv):
+    def search_contact(self, filename):
         contact = input("Enter the contact to search: ")
-        data_reader = csv.reader(data_csv, delimiter=",")
+        data_reader = csv.reader(filename, delimiter=",")
         contact_info = []
         data = ["Name", "Address", "Phone", "Email"]
+        count = 0
         for row in data_reader:
             if row[0].lower() == contact.lower():
                 contact_info = list(row)
-        print("Contact Information for contact {}: ".format(contact_info[0]))
-        for i in range(1, 4):
-            print("{type}: {info}".format(type=data[i], info=contact_info[i]))
+                count += 1
+        if count == 0:
+            print("No contact found with than name.")
+        else:
+            print("Contact Information for contact {}: ".format(contact_info[0]))
+            for i in range(1, 4):
+                print("{type}: {info}".format(type=data[i], info=contact_info[i]))
 
-    def update_contact(self):
+    def update_contact(self, filename):
         contact = input("Which contact do you want to update?: ")
         data = ["Name", "Address", "Phone", "Email"]
         updated_data = [None, None, None, None]
@@ -56,12 +61,12 @@ class ContactBook:
         for i in range(4):
             data_aux = input("Enter the new {info}: ".format(info=data[i]))
             updated_data[i] = data_aux
-        with open("contacts.csv", "r") as data_csv:
+        with open(filename, "r") as data_csv:
             data_reader = csv.reader(data_csv, delimiter=",")
             data_aux = []
             for row in data_reader:
                 data_aux.append(row)
-        with open("contacts.csv", "w") as data_csv:
+        with open(filename, "w") as data_csv:
             data_writer = csv.writer(data_csv, delimiter=",")
             for row in data_aux:
                 if row[0].lower() != contact.lower():
